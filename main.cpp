3ab5ab00db1f846d3b729427418a15e8dcf8b5f1
IS-3 <harryc142857@gmail.com>
2021-05-09 20:06:18 -0400

    wiped main.exe, will be untracked next commit

--- main.cpp
+++ main.cpp
@@ -10,46 +10,7 @@ int main(){
 
     //  ================================
 
-    int testCount;
-    cin >> testCount;
-
-    for (int currentTest = 0; currentTest < testCount; currentTest++){
-        int carCount;
-        int nextCar = 1;
-        
-        vector<int> cars;
-        stack<int> branch;
-        branch.push(0);
-        
-        cin >> carCount;
-
-        for (int i = 0; i < carCount; i++){
-            int currentCar;
-            cin >> currentCar;
-            cars.push_back(currentCar);
-        }
-
-        for (int i = carCount - 1; i >= 0; i--){
-            if (cars[i] == nextCar){
-                nextCar++;
-            }
-            else {
-                branch.push(cars[i]);
-            }
-            while (branch.top() == nextCar){
-                nextCar++;
-                branch.pop();
-            }
-        }
-
-        while (branch.top() == nextCar){
-            nextCar++;
-            branch.pop();
-        }
-
-        if (nextCar == carCount + 1) cout << "Y\n";
-        else cout << "N\n";
-    }
+    
     
     //  ================================
 }

0506e4a628231ed56e6683e574ea88db76e4791f
IS-3 <harryc142857@gmail.com>
2021-06-11 09:40:49 -0400

    shortened code

--- dmoj/uncat/avocado_trees.py
+++ dmoj/uncat/avocado_trees.py
@@ -1,33 +1,23 @@
 # avocado trees
 
-
 import sys
-from typing import List
 
 
-nqh: list = [int(x) for x in sys.stdin.readline().split(" ")]
+nqh = [int(x) for x in sys.stdin.readline().split()]
 
-n = nqh[0]
-q = nqh[1]
-h = nqh[2]
-
-sums: List[int] = []
-
-sums.append(0)
+sums = [0]
 
 best, csum = 0, 0
 
-for i in range(n):
-    ab = [int(x) for x in sys.stdin.readline().split(" ")]
-    if ab[0] <= h:
+for i in range(nqh[0]):
+    ab = [int(x) for x in sys.stdin.readline().split()]
+    if ab[0] <= nqh[2]:
         csum += ab[1]
     sums.append(csum)
 
-for i in range(q):
-    ab = [int(x) for x in sys.stdin.readline().split(" ")]
-    a = ab[0]
-    b = ab[1]
-    if sums[b] - sums[a - 1] > best:
-        best = sums[b] - sums[a - 1]
+for i in range(nqh[1]):
+    ab = [int(x) for x in sys.stdin.readline().split()]
+    if sums[ab[1]] - sums[ab[0] - 1] > best:
+        best = sums[ab[1]] - sums[ab[0] - 1]
 
-sys.stdout.
+print(best)

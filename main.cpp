73ba0b522ea013bf94e15e439a74e83bd99851ee
aris <harryc142857@gmail.com>
2021-11-30 21:27:00 -0500

    a bit of restructuring

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// avocado trees
+// main
 
 #include <bits/stdc++.h>
 
@@ -10,28 +10,7 @@ int main(){
 
     //  ================================
 
-    int n, q, h;
-    scanf("%d%d%d", &n, &q, &h);
 
-    vector<int> sums;
-    sums.push_back(0);
-    int best = 0, csum = 0;
-
-    for (int i = 0; i < n; i++){
-        int a, b;
-        scanf("%d%d", &a, &b);
-        if (a > h) b = 0;
-        csum += b;
-        sums.push_back(csum);
-    }
-
-    for (int i = 0; i < q; i++){
-        int a, b, sum;
-        scanf("%d%d", &a, &b);
-        if (sums[b] - sums[a - 1] > best) best = sums[b] - sums[a - 1];
-    }
-
-    printf("%d\n", best);
 
     //  ================================
 }

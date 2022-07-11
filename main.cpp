1a95841d9403aef0a11798634ab2ba4f345631c3
aris chen <harryc142857@gmail.com>
2022-07-11 14:10:57 -0400

    lets try this again

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-// main
+// cf 1702b
+
+typedef long long ll;
 
 #include <bits/stdc++.h>
 
@@ -10,7 +12,46 @@ int main(){
 
     //  ================================
 
+    ll incount;
+    cin >> incount;
 
+    string curr;
+    vector<char> currlist;
+    int currcount = 0;
+    int currtotal = 0;
+
+    for (ll i = 0; i < incount; i++){
+        cin >> curr;
+
+        for (int j = 0; j < curr.size(); j++){
+            if (currcount < 3){
+                if (find(currlist.begin(), currlist.end(), curr[j]) == currlist.end()){
+                    currlist.push_back(curr[j]);
+                    currcount++;
+
+                    if (currcount == 3){
+                        currtotal++;
+                    }
+                }
+            }
+            else {
+                if (find(currlist.begin(), currlist.end(), curr[j]) == currlist.end()){
+                    currcount = 1;
+                    currlist.clear();
+                    currlist.push_back(curr[j]);
+                }
+            }
+        }
+        if (currcount != 3){
+            currtotal++;
+        }
+
+        cout << currtotal << "\n";
+
+        currtotal = 0;
+        currcount = 0;
+        currlist.clear();
+    }
 
     //  ================================
 }
